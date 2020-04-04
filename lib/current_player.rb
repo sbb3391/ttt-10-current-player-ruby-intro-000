44e10e1ebf5878f6c95d659ccc3c7f5305bca545
Sam Bequette <sbb3391@gmail.com>
2020-04-04 17:59:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board1)
   count = 0
-  board.each do |board1|
-  if board[count] == "X" || board[count] == "O"
+  board1.each do |board1|
+  if board1[count] == "X" || board[count] == "O"
   count += 1
   end
 end
